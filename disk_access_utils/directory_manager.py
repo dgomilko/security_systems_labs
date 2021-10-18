f06214a0c4c568ad65ca9892b9339ee77d30272f
dgomilko <digomilko@gmail.com>
2021-10-18 12:55:56 +0300

    additional check before chown

--- disk_access_utils/directory_manager.py
+++ disk_access_utils/directory_manager.py
@@ -1,5 +1,6 @@
 import os
 import stat
+import pwd
 import sys
 sys.path.append('../')
 from system_stats import DISK_NAME, USER_PERMISSIONS, ALL_SUBDISKS, PERMITTED_SUBDISKS
@@ -11,15 +12,21 @@ def chmod_if_not_equal(path, mode):
   if not oct(os.stat(path).st_mode) == mode:
     os.chmod(path, mode)
 
+def chown_if_not_equal(path, uid, gid):
+  cur_owner = pwd.getpwuid(os.stat(path).st_uid).pw_uid
+  if cur_owner != uid: 
+    print("dd")
+    os.chown(path, uid, gid)
+
 def check_disk():
   min_permissions = 0o42000
   create_dir_if_does_not_exist(DISK_NAME)
   for subdisk in ALL_SUBDISKS:
     path = f'{DISK_NAME}/{subdisk}'
     create_dir_if_does_not_exist(path)
-    os.chown(path, 0, 0)
+    chown_if_not_equal(path, 0, 0)
     chmod_if_not_equal(path, min_permissions)
-  os.chown(path, 0, 0)
+  chown_if_not_equal(DISK_NAME, 0, 0)
   chmod_if_not_equal(DISK_NAME, min_permissions)
 
 def grant_access(user):
