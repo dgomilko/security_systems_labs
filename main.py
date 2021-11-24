34d38035f743fc54e8541f829d1a49ac5bbbb1ed
dgomilko <digomilko@gmail.com>
2021-11-24 10:46:18 +0200

    add level one threat to inactivity logout

--- main.py
+++ main.py
@@ -4,15 +4,34 @@ from session.session import init_session
 from monitor.report import init_analyzer
 from utils.directory_manager import check_disk
 from utils.logout import logout
-from utils.inactivity_logout import on_exit_delayed
 import keyboard
+from threading import Timer
+from system_stats import SESSION_TIMEOUT_SEC
+from utils.logout import logout
+from monitor.journal_manager import add_level_one_threat
 
 def main():
+  logged_user = '[unsigned user]'
+
+  def on_inactivity():
+    add_level_one_threat(logged_user)
+    logout()
+
+  t = Timer(SESSION_TIMEOUT_SEC, on_inactivity)
+  t.start()
+
+  def on_exit_delayed(_):
+    nonlocal t
+    t.cancel()
+    t = Timer(SESSION_TIMEOUT_SEC, on_inactivity)
+    t.start()
+
   check_disk()
   keyboard.on_press(on_exit_delayed)
   signal(SIGINT, logout)
   init_analyzer()
   user = login()
+  logged_user = user.login
   init_session(user) if user is not None else logout()
 
 if __name__ == "__main__":
